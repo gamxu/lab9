df4a15d2f2e73ff65afbba38598363d44f6b7d22
gamxu <wuttipat.did@gmail.com>
2021-12-07 16:00:12 +0700

    lab9_3 complete

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,11 +1,56 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your property: "
-"Your status = "
-"UNFRIEND"
-"FRIEND"
-"BEST FRIEND"
-"ONE-NIGHT-STAND"
-"MARRIED"
-*/
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string status;
+    int age, ppt,height;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if(age<=20){
+        cout << "Enter your height: ";
+        cin >> height;
+        
+        if(height<160){
+            status = "UNFRIEND";
+
+        }else if(height<175){
+            status = "FRIEND";
+            
+        }else{
+            cout << "Enter your property: "; 
+            cin >> ppt;
+
+            if(ppt>69000000){
+                status = "MARRIED";
+
+            }else{
+                status = "ONE-NIGHT-STAND";
+            }
+
+        }    
+                   
+    }else{
+        if(age<30){
+            cout << "Enter your property: ";
+            cin >> ppt;
+
+            if(ppt>1000000){
+                status = "BEST FRIEND";
+
+            }else{
+                status = "UNFRIEND";
+            }
+
+        }else{
+            status = "UNFRIEND";
+        }
+
+    }
+
+    cout << "Your status = " << status;
+
+    return 0;
+}
+    
